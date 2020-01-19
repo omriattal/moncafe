f658a7d99b70009302c29b9982a536d44b6356ff
omriattal <omriattal@gmail.com>
2020-01-20 01:22:33 +0200

    final commit now for real, no really

--- action.py
+++ action.py
@@ -10,13 +10,16 @@ def close_db():
     connect.commit()
     connect.close()
 
+
+atexit.register(close_db)
+
+
 def has_enough_and_quantity(cursor, product_id, activity_quantity):
     cursor.execute("""SELECT Products.quantity From Products WHERE Products.id = (?)
     """, [product_id])
     quantity = int(cursor.fetchone()[0])
     return quantity + activity_quantity >= 0, quantity
 
-atexit.register(close_db)
 
 input_config_name = sys.argv[1]
 cursor = connect.cursor()
@@ -24,13 +27,12 @@ with open(input_config_name) as input_config:
     input_config = input_config.read().splitlines()
     for line in input_config:
         line = line.split(", ")
-        has_enough , current_quantity = has_enough_and_quantity(cursor, int(line[0]), int(line[1]))
+        has_enough, current_quantity = has_enough_and_quantity(cursor, int(line[0]), int(line[1]))
         if int(line[1]) != 0 and has_enough:
             cursor.execute("""
-                INSERT INTO Activities (product_id,quantity,activator_id,date) VALUES (?,?,?,?)
-             """, [int(line[0]), int(line[1]), int(line[2]), line[3]])
-            cursor.execute("""UPDATE Products
-            SET quantity = ? WHERE id = ?""", [int(line[1]) + current_quantity, int(line[0])])
-
+                INSERT INTO Activities (product_id,quantity,activator_id,date) VALUES (?,?,?,?)""",
+                           [int(line[0]), int(line[1]), int(line[2]), line[3]])
+            cursor.execute("""UPDATE Products SET quantity = ? WHERE id = ?""",
+                           [int(line[1]) + current_quantity, int(line[0])])
 
 printdb.print_db(connect)
